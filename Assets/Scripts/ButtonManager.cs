5685f27e8342f57f94ea7a501ae19915d0b7be6a
CodeJirachi <jira.uttarapong@gmail.com>
2023-02-20 17:46:30 -0500

    more U.I for cooking gameplay progress, recipe opens

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField] GameObject recipeButton;
+    [SerializeField] GameObject setButton;
     [SerializeField] GameObject exitButton;
     [SerializeField] GameObject recipePage;
 
     public void openRecipe()
     {
         recipeButton.SetActive(false);
+        setButton.SetActive(false);
         exitButton.SetActive(true);
         recipePage.SetActive(true);
     }
@@ -18,8 +20,10 @@ public class ButtonManager : MonoBehaviour
     public void closeRecipe()
     {
         recipeButton.SetActive(true);
+        setButton.SetActive(true);
         exitButton.SetActive(false);
         recipePage.SetActive(false);
+
     }
 
 }
