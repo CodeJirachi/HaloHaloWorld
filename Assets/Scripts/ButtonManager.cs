9ed65e27e50a990395245fb3f4256708d544bfc8
CodeJirachi <jira.uttarapong@gmail.com>
2023-02-23 18:22:49 -0500

    Ingredient inventory started

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField] GameObject recipeButton;
-    [SerializeField] GameObject setButton;
+    [SerializeField] GameObject settingsButton;
     [SerializeField] GameObject exitButton;
     [SerializeField] GameObject recipePage;
 
     public void openRecipe()
     {
         recipeButton.SetActive(false);
-        setButton.SetActive(false);
+        settingsButton.SetActive(false);
         exitButton.SetActive(true);
+
+        //recipe page opens + its contents show with it dynamically (HMM) 
         recipePage.SetActive(true);
+        //recipe page must hold values that are set true or false depending on whether/ how they are toggled in VN gameplay 0
     }
 
     public void closeRecipe()
     {
         recipeButton.SetActive(true);
-        setButton.SetActive(true);
+        settingsButton.SetActive(true);
         exitButton.SetActive(false);
+
+        //recipe page closes
         recipePage.SetActive(false);
+        //ensures the contents of it, closes with it
 
     }
 
